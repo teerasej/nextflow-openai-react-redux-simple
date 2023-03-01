ce5d2f68036fd226b92e030ad29157bff3acc3d1
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-03-01 20:31:06 +0700

    Use hook to get form's data

--- src/components/promptinput/PromptInput.js
+++ src/components/promptinput/PromptInput.js
@@ -2,34 +2,47 @@ import React from 'react'
 import { Row, Col, Form, Button } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { messageAdded } from '../../redux/messageSlice';
+import { Controller, useForm } from 'react-hook-form';
 
 function PromptInput() {
 
     const dispatch = useDispatch();
+    const { control, handleSubmit, reset } = useForm();
 
-    const onSubmit = () => {
+    const onSubmit = (data) => {
 
+        console.log(data);
         dispatch(
             messageAdded({
                 sender: 'User',
-                message: 'Test'
+                message: data.message
             })
         )
+
+        reset();
     };
 
 
     return (
         <Row>
             <Col>
-                <Form>
+                <Form onSubmit={handleSubmit(onSubmit)}>
                     <Form.Group controlId="message">
                         <Form.Label>Message</Form.Label>
-                        <Form.Control type="text" placeholder="Type your message here"/>
+                        <Controller
+                            name="message"
+                            control={control}
+                            defaultValue=""
+                            rules={{ required: true }}
+                            render={({ field, fieldState: { error } }) => (
+                                <Form.Control type="text" placeholder="Type your message here" isInvalid={!!error} {...field} />
+                            )}
+                        />
                         <Form.Control.Feedback type="invalid">
                             Please enter a message.
                         </Form.Control.Feedback>
                     </Form.Group>
-                    <Button variant="primary" onClick={onSubmit}>
+                    <Button variant="primary" type="submit">
                         Send
                     </Button>
                 </Form>
